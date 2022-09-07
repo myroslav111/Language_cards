b893914b4c5f758f88806ce699d6e0a8bdb646e3
myroslav <myrkozar@gmail.com>
2022-09-07 22:42:43 +0300

    add footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,17 @@ import Navigation from './Navigation/Navigation';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 // import { CSSTransition } from 'react-transition-group';
-
 import './App.css';
 import AnimateRoutes from './AnimateRoutes';
+import FooterMobile from './FooterMobile';
 
 export const App = () => {
   return (
-    <>
+    <div style={{ height: '100%' }}>
       <Navigation />
       <AnimateRoutes />
+      <FooterMobile />
       <ToastContainer autoClose={3000} />
-    </>
+    </div>
   );
 };
