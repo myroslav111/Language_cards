c9a1bd7dc2775b963528d276ceebd5bc63ad70c6
myroslav <myrkozar@gmail.com>
2022-09-10 23:59:40 +0300

    fix icon i

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { SwitchTransition, CSSTransition } from 'react-transition-group';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
-import PrivacyTipIcon from '@mui/icons-material/PrivacyTip';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import api from 'service/api';
@@ -11,7 +10,6 @@ import './Card.css';
 import SoundButton from 'components/SoundButton/SoundButton';
 import DeleteWord from 'components/DeleteWord';
 import RemoveWord from 'components/RemoveWord';
-import ModalInfo from 'components/ModalInfo';
 
 function Card() {
   const [state, setState] = useState(true);
@@ -20,9 +18,6 @@ function Card() {
   const [userObj, setUserObj] = useState(null);
   const [email] = useState(localStorage.getItem('email') || '');
   const [loader, setLoader] = useState(false);
-
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
 
   useEffect(() => {
     (async () => {
@@ -113,15 +108,6 @@ function Card() {
           <span>{word.length} - Words</span>
         </div>
       </div>
-      {/* info icon */}
-      {!email && (
-        <div className="info__card" onClick={handleOpen}>
-          <PrivacyTipIcon fontSize="large" />
-        </div>
-      )}
-
-      {/* modal */}
-      <ModalInfo onOpen={open} onSetOpen={setOpen} />
     </>
   );
 }
