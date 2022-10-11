87b8727d6ebb5035cb80dc6e3436bc9b9996e050
Maksimchik36 <Peltek1985@gmail.com>
2022-10-11 22:12:02 +0300

    change
    api-key

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -3,10 +3,10 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 
 
-// firebase data config
+// firebase data
 export const firebaseConfig = {
-  // apiKey: 'AIzaSyAZWL6jBhtQG0JPHEylLfigAUp0NuMMCN0',
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: 'AIzaSyAZWL6jBhtQG0JPHEylLfigAUp0NuMMCN0',
+    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: 'lang-cards-1e9e8.firebaseapp.com',
   // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: 'lang-cards-1e9e8',
